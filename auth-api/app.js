225401a2f1912b07731924449b5b21fb5e23c0ba
MAntonioST <marco.teixeira@fatec.sp.gov.br>
2022-05-29 12:10:12 -0300

    Add jwt token

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,20 +1,40 @@
-import express from 'express'
-import * as db from '../src/config/db/initialData';
+import express from "express";
+
+import { createInitialData } from "./scr/config/db/initialData.js";
+import userRoutes from "./scr/modules/user/routes/UserRoutes.js";
+
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
+const CONTAINER_ENV = "container";
 
-db.createInitialData();
-
-app.get('/api/status',(req, res) =>{
-    return res.status(200).json({
-        service: "Auth-API",
-        status: "up",
-        httpStatus: 200
-    });
+app.get("/api/status", (req, res) => {
+  return res.status(200).json({
+    service: "Auth-API",
+    status: "up",
+    httpStatus: 200,
+  });
 });
 
+app.use(express.json());
+
+startApplication();
+
+function startApplication() {
+  if (env.NODE_ENV !== CONTAINER_ENV) {
+    createInitialData();
+  }
+}
+
+app.get("/api/initial-data", (req, res) => {
+  createInitialData();
+  return res.json({ message: "Data created." });
+});
+
+//app.use(tracing);
+app.use(userRoutes);
+
 app.listen(PORT, () => {
-  console.info(`Server started successfully at port ${PORT}`)
+  console.info(`Server started successfully at port ${PORT}`);
 });
